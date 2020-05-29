2c2b9c850e6230f312cdcc1b590d9ea9e212d108
malachishy <malachi.shy@gmail.com>
2020-05-29 12:29:01 -0400

    Refactored getCurrentLocation and moved it to location.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
+// This method should:
+//  1. Create a new Location object.
+// This means an object of class Location.
+//  2. Call the getCurrentLocation() method.
+//  3. Print the values stored inside latitude and longitude.
 void getLocation() async {
-  Position position = await Geolocator()
-      .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-  print(position);
+  Location location = Location();
+  await location.getCurrentLocation();
+  print(location.latitude);
+  print(location.longitude);
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: () {},
           child: Text('Get Location'),
         ),
       ),
