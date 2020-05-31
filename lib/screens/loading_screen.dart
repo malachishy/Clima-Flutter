124a1d50f239cb453ede1629f7c9ea3f3f5ceae8
malachi <malachi.shy@gmail.com>
2020-05-31 17:58:23 -0400

    Added Navigator method and print(lat/long) functions for testing

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:clima/screens/location_screen.dart';
 
 const String apiKey = '739d568c0baa232676ff5e8372ce16ec';
 
@@ -26,19 +27,29 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
+    //Gets the user's current location.
     Location location = Location();
     await location.getCurrentLocation();
     latitude = location.latitude;
     longitude = location.longitude;
+    print(latitude);
+    print(longitude);
 
-//This line demonstrates why it's good practice to make a variable out of classes.
-//Otherwise, I would have to paste the url property each time I wanted to create a NetworkHelper object.
-
+    //Creates a NetworkHelper object based on the long/lat from getCurrentLocation() call.
+    //This line demonstrates why it's good practice to make a variable out of classes.
+    //Otherwise, I would have to paste the url property each time I wanted to create a NetworkHelper object.
     NetworkHelper networkHelper = NetworkHelper(
         url:
             'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+    //Switches to the LocationScreen() route.
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(),
+      ),
+    );
   }
 
   @override
