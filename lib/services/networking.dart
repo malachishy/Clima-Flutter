84a991c88fad518bc802f9c56411c8ea3f7ef502
malachi <malachi.shy@gmail.com>
2020-05-31 17:24:54 -0400

    Added comments to networking.dart

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+//This class is very flexible because its functionality is not limited to only weather data.
+//It can be used for any networking capability we need in the future.
+//It would be smart to save this file for future reference.
+
 class NetworkHelper {
   final String url;
 
